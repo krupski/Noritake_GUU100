1a0ab8c74ab0681efe1a011828bc75c4b2f690b0
Roger A. Krupski <rakrupski@verizon.net>
2020-01-18 16:58:26 -0500

    Updated

---
+++ fonts/allFonts.h
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  File "allFonts.h" for the Noritake GU128X64E-U100 VFD display
+//  Copyright (c) 2012, 2017 Roger A. Krupski <rakrupski@verizon.net>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Include this file along with the Noritake_VFD_GUU100.h file to easily
+//  allow access to any font. Including this file will NOT waste any memory
+//  only the fonts you actually use take up space - the rest are not loaded.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef ALL_FONTS_H
+#define ALL_FONTS_H
+
+#include "ati_8x16.h"
+#include "coco3.h"
+#include "com64.h"
+#include "deco_8x16.h"
+#include "dutch.h"
+#include "font_5x7_d.h"
+#include "font_5x7.h"
+#include "font_5x8_d.h"
+#include "font_5x8.h"
+#include "font_6x8.h"
+#include "font_6x8x.h"
+#include "freedos_16.h"
+#include "glcdfont.h"
+#include "gothic_8x16.h"
+#include "hd44780u.bak"
+#include "hd44780u.h"
+#include "ibm_8x14.h"
+#include "ibm_8x16.h"
+#include "ibm_8x8.h"
+#include "imon_6x8.h"
+#include "iso8859_6x8.h"
+#include "italics_8x16.h"
+#include "mda_14.h"
+#include "moderne_8x16.h"
+#include "nums_15x31.h"
+#include "nums_24x32.h"
+#include "nums_4x6.h"
+#include "nums_5x8.h"
+#include "nums_5x8x.h"
+#include "nums_7x15.h"
+#include "nums_8x16.h"
+#include "ocr_8x13.h"
+#include "olivetti_16.h"
+#include "roman_8x16.h"
+#include "scrawl_8x16.h"
+#include "sevseg.h"
+#include "slant_8x16.h"
+#include "swiss_8x16.h"
+#include "t6963.h"
+#include "thin_8x14.h"
+#include "thin_8x16.h"
+#include "thin_8x8.h"
+#include "tseng_8x16.h"
+
+#endif // #ifndef ALL_FONTS_H
